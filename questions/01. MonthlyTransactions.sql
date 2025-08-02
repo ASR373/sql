c83c2e62be7a46f7247783d910fe8fa01d4d3c61
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2025-08-01 22:08:46 -0400

    Update 01. MonthlyTransactions.sql

--- questions/01. MonthlyTransactions.sql
+++ questions/01. MonthlyTransactions.sql
@@ -47,7 +47,15 @@ Output:
 
 
 
-
+select 
+  to_char(trans_date, 'YYYY-MM') as month,
+country,
+count(id) as trans_count,
+sum(case when state = 'approved' then 1 else 0 end) as approved_count,
+sum(amount) as trans_total_amount,
+sum(case when state = 'approved' then amount else 0 end) as approved_total_amount
+from transactions
+group by 1,2
 
 
 
