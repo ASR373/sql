a770bbfc2b883a48e310584cb1aa6a6a08060b95
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2025-08-04 10:31:28 -0400

    Create 05. sample time series.sql

---
+++ questions/05. sample time series.sql
@@ -0,0 +1,42 @@
+
+
+/*
+got this question from interview query - sample time series
+
+### 🧾 SQL Problem Statement
+
+**Background:**
+You are given a `transactions` table that records purchase transactions with date timestamps. The table includes user and product information, along with the quantity purchased and the date/time of the transaction.
+
+---
+
+### 🎯 Task:
+
+Write an SQL query to **sample every 4th row** from the `transactions` table, **ordered by the `created_at` timestamp**.
+
+---
+
+### 📂 Table Schema
+
+#### `transactions` table:
+
+| Column      | Type     |
+| ----------- | -------- |
+| id          | INTEGER  |
+| user\_id    | INTEGER  |
+| created\_at | DATETIME |
+| product\_id | INTEGER  |
+| quantity    | INTEGER  |
+
+---
+
+### 📤 Expected Output:
+
+| Column      | Type     |
+| ----------- | -------- |
+| created\_at | DATETIME |
+| product\_id | INTEGER  |
+
+*/
+
+
