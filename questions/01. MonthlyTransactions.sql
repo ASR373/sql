3d8de56bae460abdc9afc17c337155812cc9e01f
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2025-08-03 22:44:20 -0400

    Update 01. MonthlyTransactions.sql

--- questions/01. MonthlyTransactions.sql
+++ questions/01. MonthlyTransactions.sql
@@ -11,7 +11,7 @@
 +---------------+---------+
 id is the primary key of this table.
 The table has information about incoming transactions.
-The state column is an enum of typ ["approved", "declined"].
+The state column is an enum of type ["approved", "declined"].
  
 
 Write an SQL query to find for each month and country, the number of transactions and their total amount, the number of approved transactions and their total amount.
