05e0041d2e95d28980e0fa4980c74b6e873d38c0
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2025-08-18 22:47:48 -0400

    Create 13. Find the number of inspections per day.sql

---
+++ questions/13. Find the number of inspections per day.sql
@@ -0,0 +1,9 @@
+Find the number of inspections per day
+
+
+ID 9704
+
+Find the number of inspections per day.
+Output the result along with the date of the activity.
+Order results based on the activity date in the ascending order.
+
