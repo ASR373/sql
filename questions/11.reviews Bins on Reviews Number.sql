5c3309464bdcfe48af29a9196c9a6392ae43e0b6
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2025-08-18 22:21:31 -0400

    Update 11.reviews Bins on Reviews Number.sql

--- questions/11.reviews Bins on Reviews Number.sql
+++ questions/11.reviews Bins on Reviews Number.sql
@@ -34,3 +34,13 @@ review_scores_rating	double precision	Avg review rating
 … (other Airbnb fields)	…
 */
 
+SELECT
+    price,
+    CASE
+        WHEN number_of_reviews = 0 THEN 'NO'
+        WHEN number_of_reviews BETWEEN 1 AND 5 THEN 'FEW'
+        WHEN number_of_reviews BETWEEN 6 AND 15 THEN 'SOME'
+        WHEN number_of_reviews BETWEEN 16 AND 40 THEN 'MANY'
+        WHEN number_of_reviews > 40 THEN 'A LOT'
+    END AS reviews_qualification
+FROM airbnb_search_details;
