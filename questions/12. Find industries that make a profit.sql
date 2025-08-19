ab3482ede8b8810a987e3747de659fc0c6cdc519
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2025-08-18 22:41:07 -0400

    Update 12. Find industries that make a profit.sql

--- questions/12. Find industries that make a profit.sql
+++ questions/12. Find industries that make a profit.sql
@@ -28,3 +28,8 @@ Investment Services	39.5	10.3
 Beverages	43.2	9.9
 */
 
+select industry, min(sales) as min_sales, avg(profits) as avg_profit from forbes_global_2010_2014
+group by 1
+having avg(profits) >0
+order by 2
+
