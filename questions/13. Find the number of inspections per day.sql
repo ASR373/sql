470ca67cebb268bb003d4f900d5d38a4c4cdfd10
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2025-08-18 22:48:15 -0400

    Update 13. Find the number of inspections per day.sql

--- questions/13. Find the number of inspections per day.sql
+++ questions/13. Find the number of inspections per day.sql
@@ -9,4 +9,7 @@ Output the result along with the date of the activity.
 Order results based on the activity date in the ascending order.
 */
 
+select activity_date, count(*) as n_inspections from los_angeles_restaurant_health_inspections
+group by 1
+order by 1
 
