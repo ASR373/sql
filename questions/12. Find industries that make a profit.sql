42173069c6748f90b687933ccf9c1c3948239b90
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2025-08-18 22:35:40 -0400

    Update 12. Find industries that make a profit.sql

--- questions/12. Find industries that make a profit.sql
+++ questions/12. Find industries that make a profit.sql
@@ -1,1 +1,27 @@
+Find all industries with a positive average profit. For those industries extract their lowest sale.
+Output the industry along with the corresponding lowest sale and average profit.
+Sort the output based on the lowest sales in ascending order.
 
+Table
+forbes_global_2010_2014
+
+
+assets: double precision
+company: text
+continent: text
+country: text
+industry: text
+marketvalue: double precision
+profits: double precision
+rank: bigint
+sales: double precision
+sector: text
+
+expected output:
+
+industry	min_sales	avg_profit
+Major Banks	27.7	11.9
+Electric Utilities	32	1.27
+Software & Programming	37.9	16.95
+Investment Services	39.5	10.3
+Beverages	43.2	9.9
