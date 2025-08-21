d72ae3d4fef3ef4ad9532ed4de15c28056f64e4d
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2025-08-20 23:16:58 -0400

    Update 16. Find all searches for accommodations with an equal number of bedrooms bathrooms.sql

--- questions/16. Find all searches for accommodations with an equal number of bedrooms bathrooms.sql
+++ questions/16. Find all searches for accommodations with an equal number of bedrooms bathrooms.sql
@@ -1,1 +1,10 @@
+/* Find all searches for accommodations where the number of bedrooms is equal to the number of bathrooms.*/
 
+
+select id, price, property_type, room_type, amenities, 
+  accommodates,	bathrooms,	bed_type,	cancellation_policy,	
+  cleaning_fee,	city,	host_identity_verified,	host_response_rate,
+  host_since,	neighbourhood,	number_of_reviews,	review_scores_rating,	
+  zipcode,	bedrooms,	beds
+  from airbnb_search_details
+  where bathrooms = bedrooms
