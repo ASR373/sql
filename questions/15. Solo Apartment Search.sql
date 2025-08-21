8ffb37f9c51447b1655c4c1dad1479e640a401cd
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2025-08-20 23:14:24 -0400

    Update 15. Solo Apartment Search.sql

--- questions/15. Solo Apartment Search.sql
+++ questions/15. Solo Apartment Search.sql
@@ -3,3 +3,10 @@ Find the search details for apartments where the property type is Apartment and 
 */
 
 
+select id, price, property_type, room_type, amenities, 
+  accommodates,	bathrooms,	bed_type,	cancellation_policy,	
+  cleaning_fee,	city,	host_identity_verified,	host_response_rate,
+  host_since,	neighbourhood,	number_of_reviews,	review_scores_rating,	
+  zipcode,	bedrooms,	beds
+from airbnb_search_details
+where property_type = 'Apartment' and accommodates = 1
