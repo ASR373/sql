99592db187bca23ec6cdaac56bdea2872f15aa94
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2025-08-20 23:26:51 -0400

    Update 17. Find searches for Los Angeles neighborhoods.sql

--- questions/17. Find searches for Los Angeles neighborhoods.sql
+++ questions/17. Find searches for Los Angeles neighborhoods.sql
@@ -1,3 +1,5 @@
 /*n Find distinct searches for Los Angeles neighborhoods. Output neighborhoods and remove duplicates */
 
 
+select distinct neighbourhood from airbnb_search_details
+where city = 'LA' and neighbourhood <> '';
