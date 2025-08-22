093d6aef49a67976fac4e3c918965f82e73522a1
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2025-08-22 19:52:02 -0400

    Update 18. 3 Bed Minimum.sql

--- questions/18. 3 Bed Minimum.sql
+++ questions/18. 3 Bed Minimum.sql
@@ -1,1 +1,6 @@
+/*
+Find the average number of beds in each neighborhood that has at least 3 beds in total.
 
+
+Output results along with the neighborhood name and sort the results based on the number of average beds in descending order.
+*/
