eeacd378b8e8742531cd3899a621d158dd6ddab1
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2025-09-02 14:15:46 -0400

    Update 19. Variable vs Fixed Rates.sql

--- questions/19. Variable vs Fixed Rates.sql
+++ questions/19. Variable vs Fixed Rates.sql
@@ -1,3 +1,7 @@
+/* Write a SQL query to show each loan and its rate type. For each loan, create two new columns: fixed and variable. If the loan has a fixed rate, put a 1 in the fixed column and a 0 in the variable column. If the loan has a variable rate, do the opposite.
+*/
+
+
 select loan_id, 
 count(case when (rate_type = 'fixed') then id
 else null end) as fixed,
