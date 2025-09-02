c00f33d1a084213971e285772d54bbbe2505fa8f
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2025-09-02 14:15:20 -0400

    Update 19. Variable vs Fixed Rates.sql

--- questions/19. Variable vs Fixed Rates.sql
+++ questions/19. Variable vs Fixed Rates.sql
@@ -1,1 +1,7 @@
-
+select loan_id, 
+count(case when (rate_type = 'fixed') then id
+else null end) as fixed,
+count(case when (rate_type = 'variable') then id
+else null end) as variable
+from submissions
+group by 1
