3513591f1d7045c32a15e0bc74fc1b4ad39e1d52
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2025-09-03 14:24:25 -0400

    Update 22. Top Streamers.sql

--- questions/22. Top Streamers.sql
+++ questions/22. Top Streamers.sql
@@ -1,1 +1,8 @@
+select user_id,
+count(case when session_type = 'streamer' then 1 end) as cos,
+count(case when session_type = 'viewer' then 1 end) as cov
+from twitch_sessions
+group by 1
+having count(case when session_type = 'streamer' then 1 end) > count(case when session_type = 'viewer' then 1 end) and count(case when session_type = 'streamer' then 1 end) > 0 and
+    count(case when session_type = 'viewer' then 1 end) > 0
 
