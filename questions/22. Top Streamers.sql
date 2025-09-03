65991b9c27c87fcaee7d07bad328092d91fe9d3a
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2025-09-03 14:24:37 -0400

    Update 22. Top Streamers.sql

--- questions/22. Top Streamers.sql
+++ questions/22. Top Streamers.sql
@@ -1,3 +1,5 @@
+List the top 3 users who accumulated the most sessions. Include only the user who had more streaming sessions than viewing. Return the user_id, number of streaming sessions, and number of viewing sessions.
+
 select user_id,
 count(case when session_type = 'streamer' then 1 end) as cos,
 count(case when session_type = 'viewer' then 1 end) as cov
