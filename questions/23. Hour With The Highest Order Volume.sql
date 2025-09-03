c0baf23abafae986d869ac18a9568000300b1aba
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2025-09-03 14:58:24 -0400

    Update 23. Hour With The Highest Order Volume.sql

--- questions/23. Hour With The Highest Order Volume.sql
+++ questions/23. Hour With The Highest Order Volume.sql
@@ -1,4 +1,20 @@
 /*
 Which hour of the day has the highest average number of orders across all recorded days? Your output should include the hour that satisfies this condition and the corresponding average number of orders per 
 hour. The "order volume" refers to the count of orders placed within each hour of the day.
-/*
+*/
+
+
+with hour_counts as(
+select date(order_timestamp_utc),
+  extract(hour from order_timestamp_utc) as hour, count(*) as count_of_orders from postmates_orders
+  group by 1,2
+  ),
+
+avg_orders as (
+  select hour, avg(count_of_orders) as avg_orderss from hour_counts
+  group by 1
+  )
+
+select * from avg_orders
+where avg_orderss = (select max(avg_orderss) from avg_orders)
+
