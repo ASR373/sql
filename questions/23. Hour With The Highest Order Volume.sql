3e30247f36114ed3d94af969105a5f8c0e71d7cd
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2025-09-03 14:49:45 -0400

    Update 23. Hour With The Highest Order Volume.sql

--- questions/23. Hour With The Highest Order Volume.sql
+++ questions/23. Hour With The Highest Order Volume.sql
@@ -1,1 +1,4 @@
-
+/*
+Which hour of the day has the highest average number of orders across all recorded days? Your output should include the hour that satisfies this condition and the corresponding average number of orders per 
+hour. The "order volume" refers to the count of orders placed within each hour of the day.
+/*
