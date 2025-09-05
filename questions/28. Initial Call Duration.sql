5e6390ec8232fd5e753d0b51a29b73f5d6a90dd4
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2025-09-05 09:45:37 -0400

    Update 28. Initial Call Duration.sql

--- questions/28. Initial Call Duration.sql
+++ questions/28. Initial Call Duration.sql
@@ -1,3 +1,6 @@
+/*
+Redfin helps clients to find agents. Each client will have a unique request_id and each request_id has several calls. For each request_id, the first call is an “initial call” and all the following calls are “update calls”.  What's the average call duration for all initial calls?
+*/
 with cte as(
 select *,
 dense_rank() over (partition by request_id order by created_on asc) as rank
