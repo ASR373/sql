4d91b6d10f2b4ae720b8c12fa4f3ea2ddd6eac00
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2025-09-05 09:44:49 -0400

    Update 28. Initial Call Duration.sql

--- questions/28. Initial Call Duration.sql
+++ questions/28. Initial Call Duration.sql
@@ -1,1 +1,8 @@
+with cte as(
+select *,
+dense_rank() over (partition by request_id order by created_on asc) as rank
+from redfin_call_tracking
+)
+select avg(call_duration) from cte
+where rank = 1
 
