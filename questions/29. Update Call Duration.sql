c51bc4018a3a7d6be14775bfc27712e4be5899af
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2025-09-05 09:47:41 -0400

    Update 29. Update Call Duration.sql

--- questions/29. Update Call Duration.sql
+++ questions/29. Update Call Duration.sql
@@ -1,1 +1,5 @@
+/*
+Redfin helps clients to find agents. Each client will have a unique request_id and each request_id has several calls. For each request_id, the first call is an “initial call” and all the following calls are “update calls”.  
+What's the average call duration for all update calls?
+*/
 
