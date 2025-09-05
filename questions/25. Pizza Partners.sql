b7bd671fd94d61001530fa02c91e7d549c3cb08c
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2025-09-04 22:31:41 -0400

    Update 25. Pizza Partners.sql

--- questions/25. Pizza Partners.sql
+++ questions/25. Pizza Partners.sql
@@ -2,3 +2,8 @@
 Which partners have ‘pizza’ in their name and are located in Boston? And what is the average order amount? Output the partner name and the average order amount.
 */
 
+select pp.name as name, avg(po.amount) as avg from postmates_orders po
+join postmates_markets pm on po.city_id = pm.id
+join postmates_partners pp on po.seller_id = pp.id
+where pm.name = 'Boston' and pp.name like '%Pizza%'
+group by 1
