54e27fc740efb923538edc5090bd2d461141c332
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2025-09-05 10:27:30 -0400

    Update 31. Consecutive Days.sql

--- questions/31. Consecutive Days.sql
+++ questions/31. Consecutive Days.sql
@@ -1,1 +1,13 @@
 
+
+
+with cte as(
+SELECT
+  user_id,
+  record_date,
+  DATEDIFF(record_date, LAG(record_date, 1) OVER (PARTITION BY user_id ORDER BY record_date)) AS a,
+  DATEDIFF(record_date, LEAD(record_date, 1) OVER (PARTITION BY user_id ORDER BY record_date)) AS b
+FROM sf_events
+)
+select user_id from cte
+where a=1 and b=-1
