75048c5b05fb9c920856331a2cebe97b8d625cf0
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2025-09-05 09:48:04 -0400

    Update 29. Update Call Duration.sql

--- questions/29. Update Call Duration.sql
+++ questions/29. Update Call Duration.sql
@@ -3,3 +3,10 @@ Redfin helps clients to find agents. Each client will have a unique request_id a
 What's the average call duration for all update calls?
 */
 
+with cte as(
+select *,
+dense_rank() over (partition by request_id order by created_on asc) as rank
+from redfin_call_tracking
+)
+select avg(call_duration) from cte
+where rank != 1
