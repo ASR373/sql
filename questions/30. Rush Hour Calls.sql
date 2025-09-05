6471e98b51f417048cfa519577f5e93520d5e555
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2025-09-05 09:58:35 -0400

    Update 30. Rush Hour Calls.sql

--- questions/30. Rush Hour Calls.sql
+++ questions/30. Rush Hour Calls.sql
@@ -1,3 +1,7 @@
+/*Redfin helps clients to find agents. Each client will have a unique request_id and each request_id has several calls. For each request_id, the first call is an “initial call” and 
+all the following calls are “update calls”.  How many customers have called 3 or more times between 3 PM and 6 PM (initial and update calls combined)?
+*/
+
 with cte as(
 select request_id, extract(hour from created_on)
 from redfin_call_tracking
