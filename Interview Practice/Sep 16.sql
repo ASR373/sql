6a049e2056e661432660058a6626c5977daae22b
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2025-09-16 23:51:48 -0400

    Update Sep 16.sql

--- Interview Practice/Sep 16.sql
+++ Interview Practice/Sep 16.sql
@@ -1,1 +1,20 @@
+#4
+select dept, avg(salary) from employees
+group by dept
+having avg(salary) > 60000
 
+#5
+select c.customerid, c.name, sum(o.amount) 
+from customer c
+left join orders o on c.customerid = o.customerid
+group by 1,2
+having sum(o.amount) > avg(o.amount)
+
+#6
+with cte as(
+select region, saleid, product, amount,
+rank() over(partition by region order by amount desc) rn
+from sales
+)
+select region, saleid, product, amount from cte
+where rn<=2
