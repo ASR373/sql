0d7bbdcdcbb5b7acfdd567dfe4cab063f4bef042
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2025-09-16 23:53:35 -0400

    Update Sep 16.sql

--- Interview Practice/Sep 16.sql
+++ Interview Practice/Sep 16.sql
@@ -1,16 +1,31 @@
-#4 Find the average salary for each department where the average salary is greater than 60,000.
+/* #4 Find the average salary for each department where the average salary is greater than 60,000.*/
 select dept, avg(salary) from employees
 group by dept
 having avg(salary) > 60000
 
-#5 Find customers who have spent more than the average total spending across all customers.
+/* 5 Find customers who have spent more than the average total spending across all customers.*/
 select c.customerid, c.name, sum(o.amount) 
 from customer c
 left join orders o on c.customerid = o.customerid
 group by 1,2
 having sum(o.amount) > avg(o.amount)
 
-#6 For each Region, return the top 2 highest sales ordered by Amount.
+  correct solution:
+
+  with totals as (
+    select c.customerid, c.name, sum(o.amount) as total_spent
+    from customers c
+    join orders o on c.customerid = o.customerid
+    group by c.customerid, c.name
+),
+overall as (
+    select avg(total_spent) as avg_spent from totals
+)
+select t.customerid, t.name, t.total_spent
+from totals t, overall o
+where t.total_spent > o.avg_spent;
+
+/*#6 For each Region, return the top 2 highest sales ordered by Amount.*/
 with cte as(
 select region, saleid, product, amount,
 rank() over(partition by region order by amount desc) rn
